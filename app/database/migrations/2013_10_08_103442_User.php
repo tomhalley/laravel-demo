730310c5dce661d6c42f5db26caec7f43cc97e52
Tom Halley <tom.halley@nccgroup.com>
2013-10-08 11:36:21 +0100

    Changed naming of migrations back to the way they were before. This was causing the migration engine to crash.

---
+++ app/database/migrations/2013_10_08_103442_User.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use \Illuminate\Database\Schema\Blueprint;
+
+class User extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('User', function(Blueprint $table)
+            {
+                $table->increments("id");
+                $table->string("Username", 64);
+                $table->string("Email", 128);
+                $table->string("Password", 128);
+                $table->string("FacebookID", 128)->nullable();
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists("User");
+    }
+}
---
+++ app/database/migrations/2013_10_08_103456_UserImageVote.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use \Illuminate\Database\Schema\Blueprint;
+
+class UserImageVote extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create("UserImageVote", function(Blueprint $table)
+            {
+                $table->increments("id");
+                $table->unsignedInteger("UserID");
+                $table->unsignedInteger("ImageID");
+                $table->tinyInteger("Vote");
+
+                $table->foreign("UserID")->references("id")->on("User");
+                $table->foreign("ImageID")->references("id")->on("Image");
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists("UserImageVote");
+    }
+}
